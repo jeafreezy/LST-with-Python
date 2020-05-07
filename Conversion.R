9c4b913e3a3468912ae90202ff9219b3324044b9
Jeafreezy <jolaiyaemmanuel@gmail.com>
2020-05-07 09:33:05 +0100

    Trying out the Arcpy package

--- Conversion.R
+++ Conversion.R
@@ -5,10 +5,13 @@ require(ncdf4)
 require(rgdal)
 
 ## Input: a netCDF file
-file.nc <- "C:/LEARNING(2020)/RESEARCH_WORK/data/NTL/VNP02DNB_NRT.A2020057.1242.001.nc"
+file.nc <- "C:\\LEARNING(2020)\\RESEARCH_WORK\\data\\NTL\\New_Study_Area\\VNP02DNB_NRT.A2020095.0130.001.nc" 
+
+
+#That is a file path--> It point the program to the location of the file you want to work wit
 
 ## Output: a GeoTIFF file
-file.tiff <- 'C:/LEARNING(2020)/RESEARCH_WORK/data/NTL/nt2.tiff'
+file.tiff <- 'C:/LEARNING(2020)/RESEARCH_WORK/data/NTL/New_Study_Area/VNP02DNB_NRT1.tif'
 
 ## Import netCDF
 r.rain <- raster(file.nc)
